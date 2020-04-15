3777415b5de75f8c789448af9e3d4c5d4f8bef57
Gonzapepe <addamogonzalo@gmail.com>
2020-04-15 12:01:16 -0300

    made homepage components and finished styling homepage

---
+++ src/pages/homepage/homepage.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import './homepage.scss'
+import DirectoryMenu from '../../components/Directory-Menu/directorymenu'
+
+const HomePage = () => (
+    <div className='homepage'>
+        <DirectoryMenu />
+    </div>
+)
+
+export default HomePage
