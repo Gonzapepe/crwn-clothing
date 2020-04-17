bd6702cb66297289b83a5fca121404fbdfc8b761
Gonzapepe <addamogonzalo@gmail.com>
2020-04-17 09:08:18 -0300

    adding styles to the shop page

--- src/components/collection-preview/collection-preview.jsx
+++ src/components/collection-preview/collection-preview.jsx
@@ -1,4 +1,6 @@
 import React from 'react'
+import CollectionItem from '../collection-item/collection-item'
+import './collection-preview.scss'
 
 const CollectionPreview = ({ title, items }) => (
     <div className='collection-preview'>
@@ -6,10 +8,8 @@ const CollectionPreview = ({ title, items }) => (
             {title.toUpperCase()}
         </h1>
         <div className='preview'>
-            {items.filter((item, idx) => idx < 4).map( ({ id, name }) => (
-                <div key={id}>
-                    {name}
-                </div>
+            {items.filter((item, idx) => idx < 4).map( ({ id, ...otherItemProps }) => (
+                <CollectionItem key={id} {...otherItemProps} />
             ))}
         </div>
     </div>
