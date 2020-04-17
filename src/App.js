8cbbe289bebdb50e18e7a5fcad1638d65d1a7cce
Gonzapepe <addamogonzalo@gmail.com>
2020-04-17 00:10:36 -0300

    Added Shop Page and filtered items array

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import HomePage from './pages/homepage/homepage'
 import './App.css';
 import { Route, Switch } from 'react-router-dom'
+import ShopPage from './pages/shop/shop'
 
 
 function App() {
@@ -9,6 +10,7 @@ function App() {
     <div className="App">
       <Switch>
         <Route exact path='/' component={HomePage} />
+        <Route exact path='/shop' component={ShopPage} />
       </Switch>
     </div>
   );
